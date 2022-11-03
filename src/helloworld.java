5d9d8b47948806ea46c0f2782042ac6c3f7a6201
Piyush Srivastava <piyush.srivastava@3pillarglobal.com>
2022-11-03 23:17:39 +0530

    commit 1

---
+++ src/helloworld.java
@@ -0,0 +1,12 @@
+public class helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        int fst = 20;
+        int scnd = 40 * 2;
+        int thrd = 50;
+        int tot = fst + scnd + thrd;
+        System.out.println(tot);
+        int last = -tot + 1000;
+        System.out.println(last);
+    }
+}
